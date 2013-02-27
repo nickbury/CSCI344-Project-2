e83b99cabc793f010a3a3c015f62360db9317937
Nick Bury <nmbury@gmail.com>
2013-02-27 18:08:47 -0500

    Add twitter stream

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -3,12 +3,17 @@
 
   var textext = $("#textarea").textext({ plugins : "tags" });
   var searchString, i, searchArray;
+  var twitter = new ctwitter.CTwitter();
 
   $("#search").bind("click", function () {
     searchString = textext.textext()[0].hiddenInput().val();
     searchString = searchString.replace(/(["\[\]])/ig, "");
     searchArray = searchString.split(/\,/);
-    $("#output").html(searchArray[1]);
+    twitter.stream("statuses/filter", {lang: "en", track: searchArray}, function (stream) {
+      stream.on("data", function (tweet) {
+        $("#output").append("<p>" + tweet.text + "</p>");
+      });
+    });
   });
 
 }());
