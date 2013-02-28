581816e394dcd9cd167391fb2a7a58af5883865b
Nick Bury <nmbury@gmail.com>
2013-02-28 18:45:22 -0500

    Make JSLint and HMTL Lint happy.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,15 +1,15 @@
 (function () {
   "use strict";
+  /*global window*/
+  /*global ctwitter*/
 
-  var textext = $("#textarea").textext({ plugins : "tags" });
-  var searchString, i = 0, searchArray;
+  var $ = window.$, textext = $("#textarea").textext({ plugins : "tags" }), searchString, i = 0, searchArray;
 
   function twitterStreamConstructor(searchterms, childnum) {
     var twitter = new ctwitter.CTwitter();
     twitter.stream("statuses/filter", {lang: "en", track: searchterms}, function (stream) {
       stream.on("data", function (tweet) {
-        $(".output" + childnum).prepend("<div class='response'>" + tweet.text 
-          + "</div>");
+        $(".output" + childnum).prepend("<div class='response'>" + tweet.text + "</div>");
         $(".response").fitText(1.1, {minFontSize: 10, maxFontSize: '40px' });
         $(".response").fadeIn(250).delay(3250).fadeOut(200, function () {
           $(this).remove();
@@ -25,9 +25,8 @@
     if (i !== 0) {
       $(".output" + i).remove();
     }
-    i++;
+    i = i + 1;
     $(".output").append("<p class='output" + i + "'></p>");
     twitterStreamConstructor(searchArray, i);
   });
-
 }());
