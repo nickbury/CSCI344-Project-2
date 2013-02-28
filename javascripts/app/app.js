a313ef07de0e771a12a402cc82bbefd3aadf6f72
Nick Bury <nmbury@gmail.com>
2013-02-28 16:09:15 -0500

    Create handler for search terms

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -2,18 +2,27 @@
   "use strict";
 
   var textext = $("#textarea").textext({ plugins : "tags" });
-  var searchString, i, searchArray;
-  var twitter = new ctwitter.CTwitter();
+  var searchString, i = 0, searchArray;
+
+  function twitterStreamConstructor(searchterms, childnum) {
+    var twitter = new ctwitter.CTwitter();
+    twitter.stream("statuses/filter", {lang: "en", track: searchArray}, function (stream) {
+      stream.on("data", function (tweet) {
+        $(".output" + childnum).append("<p>" + tweet.text + "</p>");
+      });
+    });
+  }
 
   $("#search").bind("click", function () {
     searchString = textext.textext()[0].hiddenInput().val();
     searchString = searchString.replace(/(["\[\]])/ig, "");
     searchArray = searchString.split(/\,/);
-    twitter.stream("statuses/filter", {lang: "en", track: searchArray}, function (stream) {
-      stream.on("data", function (tweet) {
-        $("#output").append("<p>" + tweet.text + "</p>");
-      });
-    });
+    if (i !== 0) {
+      $(".output" + i).remove();
+    }
+    i++;
+    $(".output").append("<p class='output" + i + "'></p>");
+    twitterStreamConstructor(searchArray, i);
   });
 
 }());
